59ba79dc2f4d72f9d1c3ce620b7b8d92d8b0c785
vancelot7789 <a0952072007@gmail.com>
2018-05-01 23:43:30 +0800

    login with usr_name and usr_password

--- self_develop/mainsite/views.py
+++ self_develop/mainsite/views.py
@@ -6,8 +6,21 @@ from django.template.loader import get_template
 from django.http import HttpResponse, HttpResponseRedirect
 def login(request):
     template = get_template('login.html')
+    user_id = "123"
+    user_password = "345"
+    try:
+        user_id = request.GET['usr_id']
+        user_password = request.GET['usr_pass']
+        #print(user_id)
+        #print(user_password)
+    except:
+        user_id = None
+        user_password = None
+    #print(user_id)
+    #print(user_password)
     html = template.render(locals())
     return HttpResponse(html)
+
 def student(request):
     template = get_template('student.html')
     html = template.render(locals())

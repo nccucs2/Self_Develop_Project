9ce2e4a3776b8a9e2a184b0b921078616630817a
vancelot7789 <a0952072007@gmail.com>
2018-05-01 22:13:00 +0800

    login has made

--- self_develop/mainsite/views.py
+++ self_develop/mainsite/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.core.mail import EmailMessage
+from django.template import RequestContext
+from django.template import Context, Template
+from django.template.loader import get_template
+from django.http import HttpResponse, HttpResponseRedirect
+def login(request):
+    template = get_template('login.html')
+    html = template.render(locals())
+    return HttpResponse(html)
+def student(request):
+    template = get_template('student.html')
+    html = template.render(locals())
+    return HttpResponse(html)
 # Create your views here.
